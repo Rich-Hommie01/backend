f0cd57eda080a93d4e9ca98478d5946571734451
Rich-Hommie <richhommie160@aol.com>
2024-09-14 16:14:02 -0700

    i am comming back here

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,10 +47,13 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
+    
     if (!user) {
       return res.status(400).json({ success: false, message: "Invalid credentials" });
     }
+
     const isPasswordValid = await bcryptjs.compare(password, user.password);
+
     if (!isPasswordValid) {
       return res.status(400).json({ success: false, message: "Invalid credentials" });
     }
