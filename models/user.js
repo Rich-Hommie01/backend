0e4442cd3610c8f31de9d435727ee058527f68de
Rich-Hommie <richhommie160@aol.com>
2024-09-15 05:07:17 -0700

    pushed

--- models/user.js
+++ models/user.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
+    balance: {
+      type: Number,
+      default: 0, // Set the initial balance to zero
+    },
     email: {
       type: String,
       required: true,
