798cbf5dfd347007e52145f273f62362dc6c75cc
Rich-Hommie <richhommie160@aol.com>
2024-09-15 07:13:54 -0700

    maybe this

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,8 @@
 import express from "express";
-import {
-  login,
-  logout,
-  signup,
-  forgotPassword,
-  resetPassword,
-  checkAuth
-} from "../controllers/auth.js";
-import {
-  signupValidationRules,
-  validateSignup,
-  loginValidationRules,
-  validateLogin
-} from "../validators/auth.js";
+import { login, logout, signup, forgotPassword, resetPassword, checkAuth } from "../controllers/auth.js";
+import { signupValidationRules, validateSignup, loginValidationRules, validateLogin } from "../validators/auth.js";
 import { verifyToken } from "../middleware/verifyToken.js";
+import { updateBalance } from "../controllers/user.js";
 
 const router = express.Router();
 
@@ -21,6 +10,7 @@ router.get("/check-auth", verifyToken, checkAuth);
 
 router.post("/signup", signupValidationRules(), validateSignup, signup);
 router.post("/login", loginValidationRules(), validateLogin, login);
+router.put("/balance", updateBalance); // Create an endpoint to update the balance
 router.post("/logout", logout);
 
 router.post("/forgot-password", forgotPassword);
