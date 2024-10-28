228d96a214f17ae77b4f1a6101f07867f21be3b2
Rich-Hommie01 <richhommie544@yahoo.com>
2024-10-28 19:54:17 -0400

    nothing here

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['https://vfcu.onrender.com', 'http://localhost:3000', 'http://192.168.100.133:3000'],
+  origin: ['https://valyx.onrender.com', 'http://localhost:3000',],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
